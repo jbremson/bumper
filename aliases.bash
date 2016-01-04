06d2324f69fb360613754bd7ad1d43449fa3d20a
Joel <joel3000@gmail.com>
2016-01-03 23:14:33 -0800

    Added aliases.bash and modded setup.sh to include it.

---
+++ aliases.bash
@@ -0,0 +1,4 @@
+# Standard Aliases
+alias hgrep='history | grep $1'
+alias exp='cat ~/.bashrc | grep $1'
+alias pow='source ~/.bashrc'
--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 cp ./bmp_src.bash ~/.bmp_src
 echo ${HOME} > ~/.bmp_persist
 
+
+val=`grep '#Standard Aliases' ~/.bashrc`
+if [ -z "$val"]; then
+	cat ./aliases.bash >> ~/.bashrc
+fi
+
 # Edit .bashrc to load bmp_src, but only do it 
 # if it is not already there.
 
@@ -10,3 +16,4 @@ val=`grep 'source ~/.bmp_src' ~/.bashrc`
 if [ -z "$val" ]; then
 	echo "source ~/.bmp_src" >> ~/.bashrc
 fi
+
